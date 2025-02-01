ef355e6742a1f2d51cbe4a34a65816e954813a29
Adam Lawson <alaws057@uottawa.ca>
2025-02-01 12:27:59 -0800

    Update popup stylin

--- src/chrome-extension/popup/index.tsx
+++ src/chrome-extension/popup/index.tsx
@@ -1,6 +1,11 @@
-
 import "../global.css";
+import OptionsLink from "./settingsButton";
 
 export const Popup = () => {
-  return <div className="text p-10 font-extrabold">Hello World!</div>;
+  return (
+    <div className="text-xl font-bold text-gray-800 grid justify-center align-center bg-gray-50">
+      <h1 className="font-bold mb-5">LinkedIn Jobs Enhanced</h1>
+      <OptionsLink />
+    </div>
+  );
 };
